4f937857ff877a2dd86f129a7feb8bb20b065a0a
Justin Barber <barber.justin@gmail.com>
2017-09-29 13:22:30 -0400

    Mutations
    createLink mutation

--- lib/h2gql_web/schema.ex
+++ lib/h2gql_web/schema.ex
@@ -9,6 +9,15 @@ defmodule H2gqlWeb.Schema do
     field :description, non_null(:string)
   end
 
+  mutation do
+    field :create_link, :link do
+      arg :url, non_null(:string)
+      arg :description, non_null(:string)
+
+      resolve &H2gqlWeb.NewsResolver.create_link/3
+    end
+  end
+
   query do
     field :all_links, non_null(list_of(non_null(:link))) do
       resolve &H2gqlWeb.NewsResolver.all_links/3
